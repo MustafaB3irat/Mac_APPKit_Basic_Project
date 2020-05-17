7648434c36e9a21373397d238368f1744311ab66
Mustafa A. Bi'rat <mustafaadwi@gmail.com>
2020-05-17 21:02:26 +0300

    Initial Commit

---
+++ UIKit Basic Project 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKit Basic Project 1
+//
+//  Created by Asal Macbook 1 on 17/05/2020.
+//  Copyright © 2020 Mustafa Birat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
