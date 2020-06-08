025d916dc74453521d20f827573c4b2203bf7c5c
Mustafa B'irat <mbirat@asaltech.com>
2020-06-08 12:06:34 +0300

    Initial Commit

---
+++ Basic Mac App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Basic Mac App
+//
+//  Created by Asal Macbook 1 on 08/06/2020.
+//  Copyright © 2020 Mustafa Birat. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
